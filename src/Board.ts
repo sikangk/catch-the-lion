a05fb286b85cbeda68acad3ef2308a5361b5c3a5
sikangk <updream4@naver.com>
2022-08-23 21:09:48 +0900

    feat: create Board

--- src/Board.ts
+++ src/Board.ts
@@ -11,7 +11,7 @@ export class Cell {
 
     constructor(
         public readonly position: Position,
-        private piece: Piece
+        private piece: Piece,
     ) {
         this._el.classList.add('cell');
     }
@@ -50,5 +50,45 @@ export class Board {
 
     constructor() {
         this._el.className = 'board';
+
+        for (let row = 0; row < 4; row++) {
+            const rowEl = document.createElement('div');
+            rowEl.className = 'row';
+            this._el.appendChild(rowEl);
+
+            for (let col = 0; col < 3; col++) {
+                const cell = new Cell({ row, col }, null);
+                this.cells.push(cell);
+                rowEl.appendChild(cell._el);
+            }
+
+        }
+    }
+    render() {
+        this.cells.forEach(v => v.render());
+    }
+}
+
+
+export class DeadZone {
+    private cells: Cell[] = [];
+    readonly deadzoneEl = document.getElementById(`${this.type}_deadzone`)?.querySelector('.card-body');
+
+    constructor(public type: 'upper' | 'lower') {
+        for (let col = 0; col < 4; col++) {
+            const cell = new Cell({ col: col, row: 0 }, null);
+            this.cells.push(cell);
+            this.deadzoneEl?.appendChild(cell._el);
+
+        }
+    }
+    put(piece: Piece) {
+        const emptyCell = this.cells.find(v => v.getPiece() === null);
+        emptyCell?.put(piece);
+        emptyCell?.render();
+    }
+
+    render() {
+        this.cells.forEach(v => v.render());
     }
 }
